f347e991b2296bc69cc9836b8f0b2d5211c8aab2
Magdalena <magdalena.g156@gmail.com>
2017-09-04 15:29:07 +0200

    Api fm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { IndexComponent } from './index/index.component';
+import { FormsModule }   from '@angular/forms';
+import {HttpModule} from '@angular/http'; //added for api
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule //added for api
   ],
   providers: [],
   bootstrap: [AppComponent]
